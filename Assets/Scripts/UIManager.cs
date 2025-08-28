d628309b94e1a5b2fb1a1e8f85f97499bb9466ad
Hasan Haswary <88339205+hasanhaswary@users.noreply.github.com>
2025-08-28 15:52:51 +0200

    Login Screen Update

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static object Instance { get; internal set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
